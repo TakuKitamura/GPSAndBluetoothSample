47626740f1419e54a310934049586179107fd318
TakuKitamura <takukitamura.io@gmail.com>
2021-04-24 10:20:18 +0900

    add metatag

--- wifi_access_point/wifi_access_point.ino
+++ wifi_access_point/wifi_access_point.ino
@@ -10,41 +10,50 @@ BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 std::string writeValue = "<h1>No Data...</h1>";
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 /* TCP server at port 80 will respond to HTTP requests */
 WiFiServer server(80);
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic *pCharacteristic) {
+class MyCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
     std::string value = pCharacteristic->getValue();
-    
-    if (value.length() > 0) {
+
+    if (value.length() > 0)
+    {
       writeValue = value;
       Serial.println("*********");
       Serial.print("New value: ");
       for (int i = 0; i < value.length(); i++)
-        Serial.print(value[i]); Serial.println();
-       Serial.println("*********");
-     }
-   }
-    void onRead(BLECharacteristic *pCharacteristic) {
-      pCharacteristic->setValue(writeValue);
+        Serial.print(value[i]);
+      Serial.println();
+      Serial.println("*********");
     }
+  }
+  void onRead(BLECharacteristic *pCharacteristic)
+  {
+    pCharacteristic->setValue(writeValue);
+  }
 };
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   BLEDevice::init("BLE");
@@ -57,12 +66,11 @@ void setup() {
 
   // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
 
   pCharacteristic->setCallbacks(new MyCallbacks());
   pCharacteristic->addDescriptor(new BLE2902());
@@ -76,10 +84,12 @@ void setup() {
   //アクセスポイントを起動する
   WiFi.softAP("ESP32ap", "");
 
-  if (!MDNS.begin("esp32")) {
-      while(1) {
-          delay(1000);
-      }
+  if (!MDNS.begin("esp32"))
+  {
+    while (1)
+    {
+      delay(1000);
+    }
   }
 
   server.begin();
@@ -88,48 +98,56 @@ void setup() {
   MDNS.addService("http", "tcp", 80);
 }
 
-void loop() {
-    WiFiClient client = server.available();
-    if (!client) {
-        return;
+void loop()
+{
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+
+  if (client)
+  {
+    /* check client is connected */
+    while (client.connected())
+    {
+      /* client send request? */
+      if (client.available())
+      {
+        /* request end with '\r' -> this is HTTP protocol format */
+        String req = client.readStringUntil('\r');
+        /* now we parse the request to see which page the client want */
+        int addr_start = req.indexOf(' ');
+        int addr_end = req.indexOf(' ', addr_start + 1);
+        if (addr_start == -1 || addr_end == -1)
+        {
+          return;
+        }
+        req = req.substring(addr_start + 1, addr_end);
+
+        String s = "";
+        if (req == "/")
+        {
+          //                  IPAddress ip = client.localIP();  // サーバ側のIPアドレス
+          //                  IPAddress ip = client.remoteIP();   // クライアント側のIPアドレス
+          //                  String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+          s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+          s += "<meta charset=\"UTF-8\" http-equiv=\"refresh\" content=\"5\">";
+          s += writeValue.c_str();
+          s += "\r\n\r\n";
+          Serial.println("Sending 200");
+        }
+        else
+        {
+          /* if we can not find the page that client request then we return 404 File not found */
+          s = "HTTP/1.1 404 Not Found\r\n\r\n";
+          Serial.println("Sending 404");
+        }
+        /* send response back to client and then close connect since HTTP do not keep connection*/
+        client.print(s);
+        client.flush();
+        client.stop();
+      }
     }
-
-    if (client) {                         
-      /* check client is connected */           
-      while (client.connected()) {     
-          /* client send request? */     
-          if (client.available()) {
-              /* request end with '\r' -> this is HTTP protocol format */
-              String req = client.readStringUntil('\r');
-              /* now we parse the request to see which page the client want */
-              int addr_start = req.indexOf(' ');
-              int addr_end = req.indexOf(' ', addr_start + 1);
-              if (addr_start == -1 || addr_end == -1) {
-                  return;
-              }
-              req = req.substring(addr_start + 1, addr_end);
-
-              String s = "";
-              if (req == "/") {
-//                  IPAddress ip = client.localIP();  // サーバ側のIPアドレス
-//                  IPAddress ip = client.remoteIP();   // クライアント側のIPアドレス
-//                  String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-                  s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-                  s += writeValue.c_str();
-                  s += "\r\n\r\n";
-                  Serial.println("Sending 200");
-              }
-              else {
-                  /* if we can not find the page that client request then we return 404 File not found */
-                  s = "HTTP/1.1 404 Not Found\r\n\r\n";
-                  Serial.println("Sending 404");
-              }
-              /* send response back to client and then close connect since HTTP do not keep connection*/
-              client.print(s);
-              client.flush();
-              client.stop();
-
-            }
-        }          
-    }
+  }
 }
